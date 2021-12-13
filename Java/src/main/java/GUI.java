8cdc04b9e2bf71d73e0dbca00e41d1a4144de896
Maciej <MaciejJ1999@interia.pl>
2021-12-13 23:14:22 +0100

    Initial

---
+++ Java/src/main/java/GUI.java
@@ -0,0 +1,11 @@
+import org.openjfx;
+//public class GUI extends Application{
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        primaryStage.setTitle("My First JavaFX App");
+//
+//        primaryStage.show();
+//    }
+//
+//}
